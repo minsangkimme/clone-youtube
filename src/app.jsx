b754908a47191645ec89985d7f9a5a197b387b01
minsangkimme <minsang.tech@gmail.com>
2021-12-08 16:54:42 +0900

    add youtube service

--- src/app.jsx
+++ src/app.jsx
@@ -3,31 +3,18 @@ import VideoList from "./components/video_list/video_list";
 import styles from './app.module.css';
 import SearchHeader from './components/search_video/search_header';
 
-function App() {
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
   const search = param => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-    
-    fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${param}&type=video&key=AIzaSyDDQOa9ZNaQAysxnSsluFWWSxIelWVp2Ns`, requestOptions)
-      .then(response => response.json())
-      .then(result => result.items.map(video => ({...video, id: video.id.videoId})))
-      .then(items => setVideos(items))
-      .catch(error => console.log('error', error));
+    youtube
+        .search(param)    
+        .then(items => setVideos(items));
   }
-  
+
   useEffect(() => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-    
-    fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDDQOa9ZNaQAysxnSsluFWWSxIelWVp2Ns", requestOptions)
-      .then(response => response.json())
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error));
+    youtube
+      .mostPopular()      
+      .then(items => setVideos(items));
   }, []);
 
   return (
