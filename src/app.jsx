b933df6fdea78ab2fcc9e291e6e7e9abc721efa9
minsangkimme <minsang.tech@gmail.com>
2021-12-08 16:28:04 +0900

    add video search function

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,23 @@
 import { useEffect, useState } from "react";
 import VideoList from "./components/video_list/video_list";
+import styles from './app.module.css';
+import SearchHeader from './components/search_video/search_header';
 
 function App() {
   const [videos, setVideos] = useState([]);
-
+  const search = param => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${param}&type=video&key=AIzaSyDDQOa9ZNaQAysxnSsluFWWSxIelWVp2Ns`, requestOptions)
+      .then(response => response.json())
+      .then(result => result.items.map(video => ({...video, id: video.id.videoId})))
+      .then(items => setVideos(items))
+      .catch(error => console.log('error', error));
+  }
+  
   useEffect(() => {
     const requestOptions = {
       method: 'GET',
@@ -17,7 +31,8 @@ function App() {
   }, []);
 
   return (
-    <div>     
+    <div className={styles.app}>     
+      <SearchHeader onSearch={search} />
       <VideoList videos={videos} />
     </div>
   );
