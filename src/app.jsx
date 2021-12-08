fc6e14b55f3ffd056d3709e9af08bc819c9d5c59
minsangkimme <minsang.tech@gmail.com>
2021-12-08 15:23:07 +0900

    show video list with title

---
+++ src/app.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import VideoList from "./components/video_list/video_list";
+
+function App() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDDQOa9ZNaQAysxnSsluFWWSxIelWVp2Ns", requestOptions)
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  }, []);
+
+  return (
+    <div>     
+      <VideoList videos={videos} />
+    </div>
+  );
+}
+
+export default App;
